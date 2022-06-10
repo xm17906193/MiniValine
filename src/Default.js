e1211bd0c9990df171a4318e77d4ac78b369bd93
xiamo <xm17906193@163.com>
2022-06-10 11:17:52 +0800

    fix

--- src/Default.js
+++ src/Default.js
@@ -2,7 +2,7 @@ export const VERSION = '4.2.2'
 export const GBUrl = 'https://gravatar.loli.net/avatar/'
 export const EUrl = ['https://fastly.jsdelivr.net/npm/alus@latest', 'https://fastly.jsdelivr.net/gh/MiniValine/qq@master', 'https://fastly.jsdelivr.net/gh/MiniValine/Bilibilis@master', 'https://fastly.jsdelivr.net/gh/MiniValine/tieba@master', 'https://fastly.jsdelivr.net/gh/MiniValine/twemoji@master', 'https://fastly.jsdelivr.net/gh/MiniValine/weibo@master']
 export const pf = 'https://cdn.polyfill.io/v3/polyfill.min.js?features=es6'
-export const i18nUrl = 'https://fastly.jsdelivr.net/gh/MiniValine/minivaline-i18n@latest/'
+export const i18nUrl = 'https://fastly.jsdelivr.net/xm17906193/MiniValine/minivaline-i18n@latest/'
 export const DEHash = '9e63c80900d106cbbec5a9f4ea433a3e'
 export const MaxNL = 6
 export const PS = 6
